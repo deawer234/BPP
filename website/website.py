6b5f69f751a50e5e391ee010419f4733d2c208ea
Daniel Němec <deawer23@seznam.cz>
2023-03-28 13:41:13 +0200

    module

--- website/website.py
+++ website/website.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request, jsonify
+from model.robot_controll import sine_smooth_servo
 import pigpio
 
 website_api = Blueprint('website', __name__)
@@ -20,6 +21,9 @@ def website():
 @website_api.route('/move',  methods=["POST"])
 def move():
     data = request.get_json()  # parse JSON data from request body
+    base = data['base']
+    sine_smooth_servo(2, 1, 50, 100, base)
+    
     # do something with data
     print(data)
     response = {'message': 'Success'}
